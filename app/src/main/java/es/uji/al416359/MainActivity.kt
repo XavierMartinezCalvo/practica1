3ee25461d321675f2a3fd939ea62ae885844a7bf
Xavier Martínez Calvo <al416359@sg.uji.es>
2024-02-06 19:30:05 +0100

    Act

--- app/src/main/java/es/uji/al416359/MainActivity.kt
+++ app/src/main/java/es/uji/al416359/MainActivity.kt
@@ -1,11 +1,15 @@
 package es.uji.al416359
 
+import TennisInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import es.uji.al416359.databinding.ActivityMainBinding
+import androidx.activity.ViewModel
+import viewModelTenis
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TennisInterface {
     private lateinit var binding: ActivityMainBinding
+    private val tennisViewModel: viewModelTenis by viewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
---
+++ app/src/main/java/viewModelTenis.kt
@@ -0,0 +1,4 @@
+import androidx.lifecycle.ViewModel
+class viewModelTenis: ViewModel() {
+    var view: TennisInterface? = null
+}
